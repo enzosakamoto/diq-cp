a56127706ab4d1f96a67f468c4d69a3114d6740c
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-18 16:46:47 -0300

    fix: ajusted tailwind styles

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -2,15 +2,15 @@ import Button from '../../components/Button'
 
 export default function Login() {
   return (
-    <main className="flex w-full h-screen justify-center items-center font-montserrat bg-violet-950">
-      <form className="flex flex-col gap-8 items-center justify-center">
-        <div className="flex flex-col gap-2 justify-center">
-          <label className="text-white text-3xl">Login</label>
-          <input className="text-2xl rounded-lg p-2" type="text" />
+    <main className="flex h-screen w-full items-center justify-center bg-violet-950 font-montserrat">
+      <form className="flex flex-col items-center justify-center gap-8">
+        <div className="flex flex-col justify-center gap-2">
+          <label className="text-3xl text-white">Login</label>
+          <input className="rounded-lg p-2 text-2xl" type="text" />
         </div>
-        <div className="flex flex-col gap-2 justify-center">
-          <label className="text-white text-3xl">Senha</label>
-          <input className="text-2xl rounded-lg p-2" type="password" />
+        <div className="flex flex-col justify-center gap-2">
+          <label className="text-3xl text-white">Senha</label>
+          <input className="rounded-lg p-2 text-2xl" type="password" />
         </div>
         <Button type="submit">enviar</Button>
       </form>
