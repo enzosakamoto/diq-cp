d3515b59c625f4741bae00f8b88a497017a19891
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-18 16:45:01 -0300

    fix: add new config for tailwind prettier pluging

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  plugins: [require('prettier-plugin-tailwindcss')],
+  pluginSearchDirs: false
+};
