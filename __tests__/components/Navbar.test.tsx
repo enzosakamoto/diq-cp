7fbe0a55a1662b74872a17e4dd1df64f92679cd5
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-09-06 15:50:36 -0300

    fix: refat unit tests

--- __tests__/components/Navbar.test.tsx
+++ __tests__/components/Navbar.test.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import Navbar from '../../src/components/Navbar'
 
 import { render } from '@testing-library/react'
+import { describe, test } from 'vitest'
 import '@testing-library/jest-dom'
 
 describe('<Navbar>', () => {
-  it('should render a navbar', () => {
+  test('should render a navbar', () => {
     const { getByText, getByRole } = render(<Navbar />)
 
     expect(getByRole('navigation')).toBeInTheDocument()
