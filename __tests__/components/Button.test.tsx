7997b64062d080af9049c81e7ad190fa9cb6c898
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-09-12 15:37:31 -0300

    refat: fix button & navbar unit tests

--- __tests__/components/Button.test.tsx
+++ __tests__/components/Button.test.tsx
@@ -6,103 +6,78 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { describe, expect, test } from 'vitest'
 
-describe('<Button>', () => {
-  it('should render a button', () => {
+describe('<Button />', () => {
+  test('it should render a button', () => {
     const { getByText, getByRole } = render(<Button>Click me</Button>)
 
     expect(getByText('Click me')).toBeInTheDocument()
     expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('button')).toHaveClass(
+      'font-montserrat py-2 px-4 rounded-xl transition-all duration-500 text-white drop-shadow-md disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed bg-sky-600 hover:bg-sky-700 text-md w-28'
+    )
   })
 
-  // it('should render a button with a primary color', () => {
-  //   const { getByText } = render(<Button color="primary">Click me</Button>)
+  test('it should render a button with a primary color', () => {
+    const { getByText, getByRole } = render(<Button color="primary">Click me</Button>)
 
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md bg-green-600 hover:bg-green-700'
-  //   )
-  // })
+    expect(getByText('Click me')).toBeInTheDocument()
+    expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('button')).toHaveClass(
+      'font-montserrat py-2 px-4 rounded-xl transition-all duration-500 text-white drop-shadow-md disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed bg-green-600 hover:bg-green-700 text-md w-28'
+    )
+  })
 
-  // it('should render a button with a secondary color', () => {
-  //   const { getByText } = render(<Button color="secondary">Click me</Button>)
+  test('it should render a button with a secondary color', () => {
+    const { getByText, getByRole } = render(<Button color="secondary">Click me</Button>)
 
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md bg-red-600 hover:bg-red-700'
-  //   )
-  // })
+    expect(getByText('Click me')).toBeInTheDocument()
+    expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('button')).toHaveClass(
+      'font-montserrat py-2 px-4 rounded-xl transition-all duration-500 text-white drop-shadow-md disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed bg-red-600 hover:bg-red-700 text-md w-28'
+    )
+  })
 
-  // it('should render a button with a custom className', () => {
-  //   const { getByText } = render(<Button className="bg-yellow-600">Click me</Button>)
+  test('it should render a button with small size', () => {
+    const { getByText, getByRole } = render(<Button size="sm">Click me</Button>)
 
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md bg-yellow-600'
-  //   )
-  // })
+    expect(getByText('Click me')).toBeInTheDocument()
+    expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('button')).toHaveClass(
+      'font-montserrat py-2 px-4 rounded-xl transition-all duration-500 text-white drop-shadow-md disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed bg-sky-600 hover:bg-sky-700 text-sm w-18'
+    )
+  })
 
-  // it('should render a button with a disabled state', () => {
-  //   const { getByText } = render(<Button disabled>Click me</Button>)
+  test('it should render a button with medium size', () => {
+    const { getByText, getByRole } = render(<Button size="md">Click me</Button>)
 
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toBeDisabled()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md bg-blue-600 hover:bg-blue-700 disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed'
-  //   )
-  // })
+    expect(getByText('Click me')).toBeInTheDocument()
+    expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('button')).toHaveClass(
+      'font-montserrat py-2 px-4 rounded-xl transition-all duration-500 text-white drop-shadow-md disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed bg-sky-600 hover:bg-sky-700 text-md w-28'
+    )
+  })
 
-  // it('should render a button with a disabled state and a custom className', () => {
-  //   const { getByText } = render(
-  //     <Button disabled className="bg-yellow-600">
-  //       Click me
-  //     </Button>
-  //   )
+  test('it should render a button with large size', () => {
+    const { getByText, getByRole } = render(<Button size="lg">Click me</Button>)
 
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toBeDisabled()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md bg-yellow-600 disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed'
-  //   )
-  // })
+    expect(getByText('Click me')).toBeInTheDocument()
+    expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('button')).toHaveClass(
+      'font-montserrat py-2 px-4 rounded-xl transition-all duration-500 text-white drop-shadow-md disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed bg-sky-600 hover:bg-sky-700 text-lg w-32'
+    )
+  })
 
-  // it('should render a button with a disabled state and a custom color', () => {
-  //   const { getByText } = render(
-  //     <Button disabled color="secondary">
-  //       Click me
-  //     </Button>
-  //   )
+  test('it should render a button with primary color and large size', () => {
+    const { getByText, getByRole } = render(
+      <Button color="primary" size="lg">
+        Click me
+      </Button>
+    )
 
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toBeDisabled()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md bg-red-600 hover:bg-red-700 disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed'
-  //   )
-  // })
-
-  // it('should render a button with a custom className and a custom color', () => {
-  //   const { getByText } = render(
-  //     <Button className="bg-yellow-600" color="secondary">
-  //       Click me
-  //     </Button>
-  //   )
-
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md hover:bg-red-700 bg-yellow-600'
-  //   )
-  // })
-
-  // it('should render a button with a custom className and a custom color and a disabled state', () => {
-  //   const { getByText } = render(
-  //     <Button className="bg-yellow-600" color="secondary" disabled>
-  //       Click me
-  //     </Button>
-  //   )
-
-  //   expect(getByText('Click me')).toBeInTheDocument()
-  //   expect(getByText('Click me')).toBeDisabled()
-  //   expect(getByText('Click me')).toHaveClass(
-  //     'font-medium w-24 py-2 px-4 rounded transition-all duration-500 text-white drop-shadow-md bg-yellow-600 hover:bg-red-700 disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed'
-  //   )
-  // })
+    expect(getByText('Click me')).toBeInTheDocument()
+    expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('button')).toHaveClass(
+      'font-montserrat py-2 px-4 rounded-xl transition-all duration-500 text-white drop-shadow-md disabled:opacity-50 disabled:bg-gray-600 disabled:cursor-not-allowed bg-green-600 hover:bg-green-700 text-lg w-32'
+    )
+  })
 })
--- __tests__/components/Navbar.test.tsx
+++ __tests__/components/Navbar.test.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Navbar from '../../src/components/Navbar'
 
 import { render } from '@testing-library/react'
 import { describe, test } from 'vitest'
+
 import '@testing-library/jest-dom'
 
-describe('<Navbar>', () => {
-  test('should render a navbar', () => {
-    const { getByText, getByRole } = render(<Navbar />)
+describe('<Navbar />', () => {
+  test('it should render a navbar', () => {
+    const { getByText, getByRole } = render(
+      <Router>
+        <Navbar />
+      </Router>
+    )
 
     expect(getByRole('navigation')).toBeInTheDocument()
+    expect(getByRole('navigation')).toHaveClass(
+      'fixed flex w-full flex-row items-center justify-between gap-8 bg-transparent px-8 py-4 font-montserrat drop-shadow-md'
+    )
     expect(getByText('Formulário')).toBeInTheDocument()
     expect(getByText('Empresas')).toBeInTheDocument()
 
