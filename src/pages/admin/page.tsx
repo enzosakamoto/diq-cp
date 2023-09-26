d1f61d684becd5b0285dad0108c05333b3c4b888
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-09-26 11:53:11 -0300

    refactor: refat some css

--- src/pages/admin/page.tsx
+++ src/pages/admin/page.tsx
@@ -54,7 +54,7 @@ export default function Admin() {
         <div className="mt-24 flex w-full flex-row justify-center gap-4">
           <input
             type="text"
-            className="w-1/2 rounded-lg bg-gray-300 px-4 py-2 outline-none drop-shadow-sm"
+            className="w-1/2 rounded-lg bg-gray-300 px-4 py-2 outline-none drop-shadow-sm placeholder:font-montserrat"
             placeholder="🔎 Pesquisar"
             onChange={(e) => setSearch(e.target.value)}
           />
