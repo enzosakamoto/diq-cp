066ab23555a307ceaa6e161b641d15555d373140
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-09-26 11:52:41 -0300

    chore: add new routes

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 import Admin from './admin/page'
+import Companies from './companies/page'
+import Company from './company/page'
 import Home from './home/page'
 import Login from './login/page'
 
@@ -15,6 +17,8 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/companies/:id" element={<Company />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </Router>
