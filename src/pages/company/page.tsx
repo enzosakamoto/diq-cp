da8dc8d0534922b1f2960f79c9d72aefcc5e228a
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-09-27 15:47:45 -0300

    feat: add company page responsiveness

--- src/pages/company/page.tsx
+++ src/pages/company/page.tsx
@@ -21,19 +21,19 @@ export default function Company() {
 
   return (
     <>
-      <main className="flex h-screen w-full flex-col justify-center pt-28 font-montserrat">
-        <section className="flex h-full w-full flex-row items-center justify-center gap-20">
+      <main className="flex w-full flex-col justify-center pt-12 font-montserrat md:h-screen md:pt-28">
+        <section className="flex h-full w-full flex-col items-center justify-center gap-12 px-12 py-12 md:flex-row md:gap-20 md:px-0 md:py-0">
           <a
-            className="flex h-72 w-72 transform justify-center rounded-lg bg-white transition-all duration-500 hover:scale-105"
+            className="flex h-52 w-52 transform justify-center rounded-lg bg-white transition-all duration-500 hover:scale-105 md:h-72 md:w-72"
             href={company.link}
             target="_blank"
             rel="noreferrer"
           >
             <img className="object-contain" src={company.image} alt={`Logo da ${company.name}`} />
           </a>
-          <div className="flex w-1/2 flex-col gap-8">
-            <h1 className="text-4xl font-bold">{company.name}</h1>
-            <p className="text-xl">{company.description}</p>
+          <div className="flex w-full flex-col gap-8 md:w-1/2">
+            <h1 className="text-2xl font-bold md:text-4xl">{company.name}</h1>
+            <p className="text-base md:text-xl">{company.description}</p>
           </div>
         </section>
       </main>
