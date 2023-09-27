0d85a6619012330e0a09e0b679a05683a241c6bc
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-09-27 15:06:45 -0300

    refactor: change the effect duration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,7 +62,7 @@ export default function Navbar() {
         </a>
       </div>
       <div
-        className={`fixed left-0 top-0 z-40 flex h-screen w-full transform flex-col items-center justify-center gap-8 bg-sky-800 text-2xl text-white transition-all duration-500 ${
+        className={`fixed left-0 top-0 z-40 flex h-screen w-full transform flex-col items-center justify-center gap-8 bg-sky-800 text-2xl text-white transition-all duration-300 ${
           open ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
