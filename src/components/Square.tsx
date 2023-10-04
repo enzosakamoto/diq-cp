fa77e7be2428c8e3271ae91a1396b875a9a5713d
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-10-04 12:20:22 -0300

    feat: add new square aesthetic without responsiveness

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -2,12 +2,25 @@ import { Company } from '../interfaces/company'
 
 export default function Square({ company }: { company: Company }) {
   return (
-    <a className="" href={`/companies/${company.id}`}>
-      <img
-        src={company.image}
-        className="h-40 w-40 transform rounded-t-lg border-b-2 border-sky-400 bg-white object-contain p-4 drop-shadow-md transition-all duration-500 hover:scale-105 hover:border-sky-800 sm:h-56 sm:w-56"
-        alt={`Logo da ${company.name}`}
-      />
+    <a
+      href={`/companies/${company.id}`}
+      className="relative flex flex-col justify-center overflow-hidden drop-shadow-xl"
+    >
+      <div className="absolute inset-0 bg-center drop-shadow-xl"></div>
+      <div className="group relative m-0 flex h-72 w-72 rounded-xl sm:mx-auto sm:max-w-lg">
+        <div className="z-10 h-full w-full overflow-hidden rounded-xl bg-white opacity-80 transition duration-300 ease-in-out group-hover:opacity-100">
+          <img
+            src={company.image}
+            className="animate-fade-in block h-full w-full scale-100 transform object-contain object-center p-6 opacity-100 transition duration-300 group-hover:scale-110"
+            alt=""
+          />
+        </div>
+        <div className="absolute bottom-0 z-20 m-0 pb-4 ps-4 opacity-0 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110 group-hover:opacity-100">
+          <h1 className="font-montserrat text-lg font-bold text-black">
+            {company.name.split(' ')[0]}
+          </h1>
+        </div>
+      </div>
     </a>
   )
 }
