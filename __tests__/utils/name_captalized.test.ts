8438bd7a28b4150ab3241a14099bc08fe0092012
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-10-10 16:02:37 -0300

    fix: fix unit test

--- __tests__/utils/name_captalized.test.ts
+++ __tests__/utils/name_captalized.test.ts
@@ -5,6 +5,6 @@ import { describe, expect, test } from 'vitest'
 describe('name captalized', () => {
   test('should return name captalized', () => {
     const name = 'ISSO É UM TESTE'
-    expect(nameCaptalized(name)).toBe('Isso é um Teste')
+    expect(nameCaptalized(name)).toBe('Isso é Um Teste')
   })
 })
